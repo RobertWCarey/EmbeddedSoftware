f0d501fe8c277d32c95fc7caf4002781f64ceead
Robert Carey <rob_carey_1@hotmail.com>
2019-05-06 10:12:25 +1000

    - added rtc module code, to be tested

--- Sources/RTC.c
+++ Sources/RTC.c
@@ -8,21 +8,64 @@
  *  @date 2015-08-24
  */
 
-#ifndef RTC_H
-#define RTC_H
 
 // new types
 #include "types.h"
+#include "MK70F12.h"
+
+static void (*UserFunction)(void*);
+static void* UserArguments;
 
 /*! @brief Initializes the RTC before first use.
  *
  *  Sets up the control register for the RTC and locks it.
  *  Enables the RTC and sets an interrupt every second.
  *  @param userFunction is a pointer to a user callback function.
- *  @param userArguments is a pointer to the user arguments to use with the user callback function.
+ *  @param userArguments is a pointer to the user argum	ents to use with the user callback function.
  *  @return bool - TRUE if the RTC was successfully initialized.
  */
-bool RTC_Init(void (*userFunction)(void*), void* userArguments);
+bool RTC_Init(void (*userFunction)(void*), void* userArguments)
+{
+  //Initialise local versions for userFunction and userArgument
+    UserFunction 	= userFunction;
+    UserArguments = userArguments;
+
+    // Enable RTC clock in clock gate 6
+    SIM_SCGC6 |= SIM_SCGC6_RTC_MASK;
+
+    //Disable time count
+    RTC_SR &= ~RTC_SR_TCE_MASK;
+
+    //Clear TIF and TOF by writing to TSR
+    //Check if TOF or TIF is raised before clearing TSR
+    if(RTC_SR & (RTC_SR_TOF_MASK | RTC_SR_TIF_MASK) )
+      RTC_TSR = RTC_TSR_TSR(0);
+
+    //Disable RTC Oscillator
+    RTC_CR &= ~RTC_CR_OSCE_MASK;
+
+    //Set Load Capacitance to 18PF
+    RTC_CR |= RTC_CR_SC2P_MASK;
+    RTC_CR |= RTC_CR_SC16P_MASK;
+
+    //Enable RTC Oscillator
+    RTC_CR |= RTC_CR_OSCE_MASK;
+
+    // Enable RTC Time Seconds Interrupt
+    RTC_IER |= RTC_IER_TSIE_MASK;
+
+    //Enable RTC Time Count
+    RTC_SR |= RTC_SR_TCE_MASK;
+
+    //Set up interrupt vector
+    //Vector=, IRQ=67, non-IPR=2
+    //clear any pending interrupts at RTC_seconds
+    NVICICPR2 = (1 << 3);
+    //Enable interrupts from RTC_seconds
+    NVICISER2 = (1 << 3);
+
+    return true;
+}
 
 /*! @brief Sets the value of the real time clock.
  *
@@ -31,7 +74,17 @@ bool RTC_Init(void (*userFunction)(void*), void* userArguments);
  *  @param seconds The desired value of the real time clock seconds (0-59).
  *  @note Assumes that the RTC module has been initialized and all input parameters are in range.
  */
-void RTC_Set(const uint8_t hours, const uint8_t minutes, const uint8_t seconds);
+void RTC_Set(const uint8_t hours, const uint8_t minutes, const uint8_t seconds)
+{
+  //Disable RTC Time Count
+   RTC_SR &= ~RTC_SR_TCE_MASK;
+
+   //write to RTC_TSR
+   RTC_TSR = seconds + (minutes*60) + (hours*3600);
+
+   //Enable RTC Time Count
+   RTC_SR |= RTC_SR_TCE_MASK;
+}
 
 /*! @brief Gets the value of the real time clock.
  *
@@ -40,7 +93,15 @@ void RTC_Set(const uint8_t hours, const uint8_t minutes, const uint8_t seconds);
  *  @param seconds The address of a variable to store the real time clock seconds.
  *  @note Assumes that the RTC module has been initialized.
  */
-void RTC_Get(uint8_t* const hours, uint8_t* const minutes, uint8_t* const seconds);
+void RTC_Get(uint8_t* const hours, uint8_t* const minutes, uint8_t* const seconds)
+{
+  uint32_t TSR = RTC_TSR;
+  TSR = TSR % 86400U;          /* Seconds left */
+  *hours = TSR / 3600U;     	/* Hours */
+  TSR = TSR % 3600u;           /* Seconds left */
+  *minutes = TSR / 60U;     /* Minutes */
+  *seconds = TSR % 60U;     /* Seconds */
+}
 
 /*! @brief Interrupt service routine for the RTC.
  *
@@ -48,6 +109,9 @@ void RTC_Get(uint8_t* const hours, uint8_t* const minutes, uint8_t* const second
  *  The user callback function will be called.
  *  @note Assumes the RTC has been initialized.
  */
-void __attribute__ ((interrupt)) RTC_ISR(void);
-
-#endif
+void __attribute__ ((interrupt)) RTC_ISR(void)
+{
+  // Call user function
+    if(UserFunction)
+      (*UserFunction)(UserArguments);
+}
